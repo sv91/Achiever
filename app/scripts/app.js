3fbac014d4ae5b2609c67ae3b11180108c8afce5
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-07-12 12:54:16 +0200

    Yo

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name achieverApp
+ * @description
+ * # achieverApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('achieverApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name achieverApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the achieverApp
+ */
+angular.module('achieverApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
